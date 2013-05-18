c279c774f03f249515f6f698a0efe8f2f9f7ec35
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2013-05-18 05:45:06 +0200

    Working on request video chunks

--- js/index.js
+++ js/index.js
@@ -2,14 +2,16 @@ window.addEventListener('load', function()
 {
   $('#file').change(function(event)
   {
+    document.getElementById("url").value = ""
+
     var video = document.getElementById("video")
 
 //    var reader = new FileReader();
-//    reader.onload = function(e)
-//    {
-//      video.src=reader.result;
-//    }
-//    reader.readAsDataURL(this.files[0]);
+//        reader.onload = function(e)
+//        {
+//          video.src=reader.result;
+//        }
+//        reader.readAsDataURL(this.files[0]);
 
     var video_url = window.URL.createObjectURL(this.files[0])
 
@@ -20,5 +22,78 @@ window.addEventListener('load', function()
 
     video.appendChild(source)
     video.play()
+  })
+
+  $('#url').keypress(function(event)
+  {
+    if(event.keyCode == 13)
+    {
+      // Remove local video
+      document.getElementById("file").value = ""
+
+      var video = document.getElementById("video")
+          video.src = null
+
+      // Stream video file
+      var blobs = []
+      var reqIndex = 0
+      var readIndex = 0
+
+//      var blobSize = 1024*1024
+      var blobSize = 1000*1000
+
+      function playVideo()
+      {
+        window.URL.revokeObjectURL(video.src);
+
+        video.src = window.URL.createObjectURL(blobs[readIndex])
+        video.load()
+        video.play()
+
+        readIndex++
+      }
+
+//      video.onpause = playVideo
+//      video.onended = playVideo
+      $('#resume').click(playVideo)
+
+      function sendRequest()
+      {
+        var range = 'bytes='+String(reqIndex*blobSize)+'-'+
+                             String((reqIndex+1)*blobSize-1)
+
+        var oReq = new XMLHttpRequest();
+            oReq.open("get", $('#url').val(), true);
+            oReq.responseType = "blob";
+            oReq.setRequestHeader('Range', range)
+
+        oReq.onload = function(event)
+        {
+          var index = oReq.getResponseHeader('Content-Range')
+          var patt=/\d+/g;
+
+          index = Math.floor(parseInt(patt.exec(index)) / blobSize)
+
+          blobs[index] = oReq.response;
+
+          if(video.src == "")
+          {
+            readIndex = reqIndex
+            playVideo()
+          };
+
+          reqIndex++;
+          sendRequest()
+        };
+        oReq.onerror = function(event)
+        {
+          console.error(event)
+        }
+
+        oReq.send()
+      }
+
+      sendRequest()
+    }
   })
 })
